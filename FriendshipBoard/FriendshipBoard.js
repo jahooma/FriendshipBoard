8069c88c203f0d7c367cba641f83a8f61a4b4076
randylzhao <randylzhao@gmail.com>
2013-10-07 16:50:45 -0400

    Limited the number of list objects shown

--- FriendshipBoard/FriendshipBoard.js
+++ FriendshipBoard/FriendshipBoard.js
@@ -1,7 +1,8 @@
+//To test friendshipboard on windows, go to http://127.0.0.1:3000/ instead of localhost
 Messages = new Meteor.Collection("messages");
 
 if (Meteor.isClient) {
 	Template.messages.messages = function () {
-		return Messages.find({}, {sort: {time : -1}});
+		return Messages.find({}, {limit: 10}, {sort: {time : -1}});
 	};
   };
