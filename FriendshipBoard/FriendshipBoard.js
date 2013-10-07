11ae9ba000346fb5c6671f6e8c1929ed7956290c
randylzhao <randylzhao@gmail.com>
2013-10-07 16:22:40 -0400

    Made a collection kinda work

--- FriendshipBoard/FriendshipBoard.js
+++ FriendshipBoard/FriendshipBoard.js
@@ -1,19 +1,7 @@
+Messages = new Meteor.Collection("messages");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to FriendshipBoard.";
+	Template.messages.messages = function () {
+		return Messages.find({}, {sort: {time : -1}});
+	};
   };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
