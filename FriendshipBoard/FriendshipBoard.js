1825f63032ba3243443544897545ecd42e876bbc
jahooma <jahooma@gmail.com>
2013-11-18 17:41:07 -0500

    Simple sentiment analysis page

--- FriendshipBoard/FriendshipBoard.js
+++ FriendshipBoard/FriendshipBoard.js
@@ -1,10 +1,11 @@
 Messages = new Meteor.Collection("messages");
+SentimentMessages = new Meteor.Collection("sentiment-messages");
 Stories = new Meteor.Collection("stories");
 Images = new  Meteor.Collection("images");
 LocationId = new Meteor.Collection("location");
 
 if (Meteor.isClient) {
-	var page = StoryBoardPage(); //ChatPage();
+	var page = SentimentPage(); //ChatPage();
 	Meteor.startup(function () {
 		page.startup();		
 	});
--- FriendshipBoard/sentiment-page/SentimentPage.js
+++ FriendshipBoard/sentiment-page/SentimentPage.js
@@ -1,14 +1,16 @@
+var locationId = 4;
 SentimentPage = function() {
 	var my = {};
 	var config = {
 		secondsSeparatingMessages: 1,
 		onMessageUpdate: function(message) {
-			console.log("update message ", message.id + ", is complete", message.isComplete, ":", message.text);
+			console.log("New sentiment message:", message.text);
 			if (message.isComplete) {
-				Messages.insert({
+				SentimentMessages.insert({
 					message: message.text,
 					time : message.date,
-					timeText : message.dateText 
+					timeText : message.dateText,
+					sentimentScore : analyze(message.text).score
 				});
 				Session.set("currentText", "");
 			}
@@ -25,13 +27,27 @@ SentimentPage = function() {
 	return my;
 }
 
+function changeLocation () {
+	LocationId.insert({ id : 0, time : 0});
+	locationId = LocationId.findOne({},{sort: {time: -1}}).id;
+	console.log(locationId);
+	locationId = (locationId + 1)%4;
+	LocationId.insert({ id : locationId, time : Date.now()});
+	started = true;
+}
+
 if (Meteor.isClient){
-	Template.messageHistory.messages = function () {
-		return Messages.find({},{sort: {time: -1}, limit: 10}).fetch().reverse();
+	var started = false;
+	if (!started) {
+		setTimeout(changeLocation, 1000);
+	}
+	
+	Template.sentimentMessageHistory.messages = function () {
+		return SentimentMessages.find({},{sort: {time: -1}, limit: 10}).fetch().reverse();
 	};
 
-	Template.recentMessages.messages = function () {
-		var recent = Messages.find({},{sort: {time: -1}, limit: 3}).fetch().reverse();
+	Template.sentimentRecentMessages.messages = function () {
+		var recent = SentimentMessages.find({},{sort: {time: -1}, limit: 3}).fetch().reverse();
 		recent.push({message: Session.get("currentText")});
 		var ids = ["third-most-recent", "second-most-recent", "first-most-recent", "in-progress"];
 		for(var i = 0 ; i < recent.length; i++)
